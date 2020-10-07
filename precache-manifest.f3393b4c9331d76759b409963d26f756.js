22e4336b44c5691c97def9aaa2a33ed1102c4907
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-07 16:11:51 +0000

    Deploy sraksh/redux-book-cart to github.com/sraksh/redux-book-cart.git:gh-pages

---
+++ precache-manifest.f3393b4c9331d76759b409963d26f756.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf3f2684efea58136fc796e2c515ad54",
+    "url": "/redux-book-cart/index.html"
+  },
+  {
+    "revision": "6e90c02493a9c551035d",
+    "url": "/redux-book-cart/static/css/main.0bb5df01.chunk.css"
+  },
+  {
+    "revision": "7fd853d26c3107dc2db5",
+    "url": "/redux-book-cart/static/js/2.8c7729fe.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/redux-book-cart/static/js/2.8c7729fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e90c02493a9c551035d",
+    "url": "/redux-book-cart/static/js/main.d3712ab5.chunk.js"
+  },
+  {
+    "revision": "9a4a1a9aabfab5c5d068",
+    "url": "/redux-book-cart/static/js/runtime-main.d8a1a6d9.js"
+  }
+]);
